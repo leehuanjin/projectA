a43451ef3aa1fc3151c606913871485a5b5cb9f1
chaehyun100070579 <32354521+chaehyun100070579@users.noreply.github.com>
2017-10-30 14:25:39 +0800

    admin(login,logout,profile,access_log)

---
+++ Swinburne hostel website/hostel/admin/includes/header.php
@@ -0,0 +1,43 @@
+<div class="brand clearfix">
+
+    <div id="headerwrapper">
+        <div id="header"  itemscope="itemscope" itemtype="//schema.org/WPHeader" > 
+            <div class="container ">
+                <div id="logo">
+                    <a href="https://www.swinburne.edu.my/" rel="home">
+                        <img src="../wp-content/uploads/Swinburne-Malaysia-Logo2.png" alt="Swinburne University, Malaysia">
+                    </a>
+
+
+                </div>
+                <!--/logo -->
+
+
+                <ul class="topmenu">
+                    <li><a href="https://www.swinburne.edu.my/category/library" target="_self">Library</a></li><li><a href="https://www.swinburne.edu.my/current-students" target="_self">Current Students</a></li><li><a href="https://www.swinburne.edu.my/contact" target="_self">Contact us</a></li><li><a href="https://blackboard.swinburne.edu.my/" target="_self">Blackboard Login</a></li><li></li>	        
+                </ul>
+
+
+                <span class="menu-btn"><i class="fa fa-bars"></i></span>
+                <ul class="ts-profile-nav">
+                    <li class="ts-account">
+                        <a href="#"><img src="img/ts-avatar.jpg" class="ts-avatar hidden-side" alt=""> Account <i class="fa fa-angle-down hidden-side"></i></a>
+                        <ul>
+                            <li><a href="my-profile.php">My Account</a></li>
+                            <li><a href="logout.php">Logout</a></li>
+                        </ul>
+                    </li>
+                </ul>
+
+
+
+
+            </div>
+
+        </div><!--/container -->
+
+    </div><!--/header -->
+
+
+
+</div>
