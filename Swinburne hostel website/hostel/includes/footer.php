dab56cf8493437dca7d994486b375153e580de46
chaehyun100070579 <32354521+chaehyun100070579@users.noreply.github.com>
2017-10-30 15:28:53 +0800

    Add files via upload

---
+++ Swinburne hostel website/hostel/includes/footer.php
@@ -0,0 +1,134 @@
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<div id="footer" role="contentinfo" itemscope="itemscope" itemtype="//schema.org/WPFooter" >
+    <div class="footerwrapper">
+        <hr>
+        <div class="footertop">
+            <div class="container">	
+
+
+                <div class="row">	
+                    <div class="col-xs-12 col-sm-12 col-md-8">
+
+                        <div class="col-xs-6 col-sm-6 col-md-3 footerwidget">
+                            <h5>Information for...</h5>			   		
+                        </div>
+
+                        <div class="col-xs-6 col-sm-6 col-md-3 footerwidget">
+                            <h5>Quick Links</h5>			   	
+                        </div>
+
+                        <div class="col-xs-6 col-sm-6 col-md-3 footerwidget">
+                            <h5>Key Areas</h5>			   		
+                        </div>
+
+                        <div class="col-xs-6 col-sm-6 col-md-3 footerwidget">
+                            <h5>Campuses</h5>			   		
+                        </div>
+                    </div>
+
+                    <div class="col-xs-12 col-sm-12 col-md-4 footerwidget">	
+
+                        <div class="box">
+                            <div class="box__content">
+                                <a href="https://www.google.com/maps/d/viewer?mid=1R6qbeWRFrbHK-O1dVKFdvkzzIJE&hl=en_US&ll=1.532181999999996%2C110.35728799999993&z=17" target="_blank" title="Find Swinburne University on Google Map">
+                                    <img width="400" src="wp-content/uploads/2017/02/staticmap.jpg" class="img-responsive" alt="Google Map of Swinburne Malaysia">   
+                                </a>
+                                <ul class="contact-list">
+                                    <li><span>Address</span>Jalan simpang tiga kuching, Sarawak, Malaysia</li><li><span>Tel</span>+ 21313</li><li><span>Fax</span>+3213123</li>	
+                                </ul>
+                            </div>
+                        </div>	
+
+                    </div>
+
+
+                </div><!-- row -->
+            </div><!--container-->
+        </div><!--footertop-->
+
+        <hr>
+        <div class="footermiddle">
+
+            <div class="container">									
+                <div class="row">
+
+                    <div class="col-sm-12 col-md-6">
+                        <div class="social-icons">
+
+
+                            <span class="widgetittle">Connect with us</span>
+                            <ul>
+                                <li><a class="facebook" href="https://www.facebook.com/swinburnesarawak" target="_blank" title="Like Swinburne University Malaysia on Facebook"><i class="fa fa-facebook"></i><span class="socialtitle">Facebook</span></a></li>
+                                <li><a class="twitter" href="https://twitter.com/swinburne_swk?lang=ko" target="_blank" title="Follow Swinburne University Malaysia on Twitter"><i class="fa fa-twitter"></i><span class="socialtitle">Twitter</span></a></li>
+                                <li><a class="instagram" href="https://www.instagram.com/swinburnesarawak/" target="_blank" title="Follow Swinburne University Malaysia on Instagram"><i class="fa fa-instagram"></i><span class="socialtitle">Instagram</span></a></li>
+                                <li><a class="youtube" href="https://www.youtube.com/user/swinburnesarawakweb?feature=CAQQwRs%3D" target="_blank" title="Follow Swinburne University Malaysia on YouTube"><i class="fa fa-youtube"></i><span class="socialtitle">Youtube</span></a></li>
+
+
+                            </ul></div> 
+
+                    </div>
+                    <div class="col-sm-12 col-md-6">
+
+                        <div class="footerbanner">
+
+                            <small><p>Swinburne is ranked as one of the world’s most international universities for student population diversity and international collaboration.</p>
+                            </small> 
+
+                        </div>
+
+                    </div>
+
+                </div><!-- row -->
+            </div><!--container-->
+        </div><!--footermiddle-->
+
+        <hr>
+        <div class="footerbottom">
+            <div class="container">									
+                <div class="row">
+                    <div class="col-sm-12 col-md-6">
+                        <p><small>© Swinburne Sarawak </small><br />
+                            <strong>DULN004(Q) KP(JPS)5195/IPTS/1144 Co. No. 497194-M </strong><br />
+                            <small>Last updated: Monday, 02-Oct-2017 at 9:29 AM</small><br />
+                            <small>Maintained by: Webmaster (webmaster@swinburne.edu.my)</small></p>
+                    </div>
+
+                    <div class="col-sm-6 footerbottomright">
+                        <ul class="footermenu">
+                            <li><a href="http://www.swinburne.edu.au/copyright-disclaimer/" target="_self" >Copyright and disclaimer</a></li><li><a href="https://www.swinburne.edu.my/privacy/privacy-collection-notice" target="_self" >Privacy</a></li><li>
+                            <a href="https://www.swinburne.edu.my/accessibility-information" target="_blank" >Accessibility</a></li>      
+                        </ul>	
+                        <span class="footerbutton"><a href="https://www.swinburne.edu.my/feedback" target="_self" class="button">WEBSITE FEEDBACK</a></span>          
+
+                    </div>
+
+                </div><!-- row -->
+            </div><!--container-->
+        </div><!--footerbottom-->
+
+
+
+
+    </div><!--footerwrapper-->
+</div><!-- end of footer -->
+
+<div id="back-top">
+    <a href="#top"><span class="glyphicon glyphicon-chevron-up"></span></a>
+</div>
+
+
+
+
+
