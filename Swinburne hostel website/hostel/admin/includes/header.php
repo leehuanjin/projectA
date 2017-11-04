53842593f3bf9db16726b323c399e670ec498217
samueltiong1996 <samuel_tiong@hotmail.com>
2017-11-04 20:28:19 +0800

    Update header.php

--- Swinburne hostel website/hostel/admin/includes/header.php
+++ Swinburne hostel website/hostel/admin/includes/header.php
@@ -21,7 +21,7 @@
                 <span class="menu-btn"><i class="fa fa-bars"></i></span>
                 <ul class="ts-profile-nav">
                     <li class="ts-account">
-                        <a href="#"><img src="img/ts-avatar.jpg" class="ts-avatar hidden-side" alt=""> Account <i class="fa fa-angle-down hidden-side"></i></a>
+                        <a href="#"><img src="img/SWINBURNE.jpg" class="ts-avatar hidden-side" alt=""> Account <i class="fa fa-angle-down hidden-side"></i></a>
                         <ul>
                             <li><a href="my-profile.php">My Account</a></li>
                             <li><a href="logout.php">Logout</a></li>
